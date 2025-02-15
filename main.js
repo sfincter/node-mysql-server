3ff135af9bff4963cf2257815c3977f280eeb5fa
noName <samyrize77777@gmail.com>
2025-02-16 01:50:01 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Раздача статических файлов
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Главная страница
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
