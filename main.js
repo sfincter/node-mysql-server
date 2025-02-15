49e86b1b48deefa24b9d2f7b4c43b9b4a4d09607
noName <samyrize77777@gmail.com>
2025-02-16 01:59:42 +0400

    database

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const mysql = require('mysql2');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mysql://root:LuqNCXZMEiHtvCZQWvjXMjJXkMndbCGH@interchange.proxy.rlwy.net:44057/railway
+
+
+
+// Подключение к MySQL
+const db = mysql.createConnection({
+    host: 'interchange.proxy.rlwy.net',
+    user: 'root',
+    password: 'LuqNCXZMEiHtvCZQWvjXMjJXkMndbCGH',
+    database: 'railway'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Ошибка подключения к MySQL:', err);
+        return;
+    }
+    console.log('Подключено к MySQL');
+});
 
 // Раздача статических файлов
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +35,9 @@ app.get('/', (req, res) => {
 });
 
 // Запуск сервера
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
+
+// Поддержка Railway
+module.exports = app;
