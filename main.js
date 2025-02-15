4368a46e279bd38e03ddd07df03e5e9037fbbab1
noName <samyrize77777@gmail.com>
2025-02-16 02:47:47 +0400

    fix console logs

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 // Подключение к MySQL
 const db = mysql.createConnection({
-    host: 'mysql.railway.internal',
-    user: 'root',
-    password: 'LuqNCXZMEiHtvCZQWvjXMjJXkMndbCGH',
-    database: 'railway'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
