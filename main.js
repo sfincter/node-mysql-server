12d4ef750b6e1c59b8810b65e6ed375f8a92512b
noName <samyrize77777@gmail.com>
2025-02-16 02:32:43 +0400

    Добавил логи подключения к MySQL

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+require('dotenv').config();
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
+
 // Подключение к MySQL
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
